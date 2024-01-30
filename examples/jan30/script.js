47ecba042994d861255bbb2accddf462cf20d468
Gold3n-LCP <lukepalassis@gmail.com>
2024-01-30 09:42:08 -0500

    added example from Jan30

---
+++ examples/jan30/script.js
@@ -0,0 +1,9 @@
+const doStuff = () => {
+    const messageP = document.getElementById("message");
+    messageP.innerHTML = "hello Luke";
+    medssageP.classList.toggle("special");//Make sure to take away parenthesis.
+    }
+
+const hideStrawberry = () => {
+    document.getElementById("strawberry").classList.add("hidden");
+}
