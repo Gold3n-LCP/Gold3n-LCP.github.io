1faa0bbabc21883b78accdfd16d05cc23a6b4c5e
Gold3n-LCP <lukepalassis@gmail.com>
2024-03-12 21:42:57 -0400

    uploaded assignment 12

--- CSCE242/assignments/12/script.js
+++ CSCE242/assignments/12/script.js
@@ -16,12 +16,27 @@ const showHouses = async () => {
 };
 
 const getHouse = (house) => {
-  let section = document.createElement("section");
-  section.classList.add("house");
+  let superSection = document.createElement("section");
+  superSection.setAttribute("id", "superSection");
+
+  let head = document.createElement("section");
+  head.classList.add("house");
 
   let h1 = document.createElement("h1");
   h1.innerText = house.name;
-  section.append(h1);
+  head.append(h1);
+  superSection.append(head);
+
+  let section = document.createElement("section");
+  section.setAttribute("id", "infoSection");
+  section.setAttribute("class", "columns");
+  section.classList.add("house");
+
+  let img = document.createElement("img");
+  img.src =
+    "https://portiaportia.github.io/json/images/house-plans/" +
+    house.main_image;
+  section.append(img);
 
   let h3 = document.createElement("h3");
   h3.innerText =
@@ -31,21 +46,40 @@ const getHouse = (house) => {
     house.bedrooms +
     "\n\nBathrooms: " +
     house.bathrooms +
-    "\n";
-  for (let feature in house.features) {
-    h3.append(feature);
-    console.log(feature);
-  }
+    "\n\n";
+  house.features.forEach((feat) => {
+    h3.append("* " + feat + " ");
+    console.log(feat);
+  });
   section.append(h3);
 
-  let img = document.createElement("img");
-  img.src =
-    "https://portiaportia.github.io/json/images/house-plans/" +
-    house.main_image;
-  console.log(img.src);
-  section.append(img);
+  superSection.append(section);
 
-  return section;
+  let section2 = document.createElement("section");
+  section2.setAttribute("class", "columns");
+  let levelSection = document.createElement("section");
+
+  let floorPlans = house.floor_plans;
+  floorPlans.forEach((floorPlan) => {
+    let levelSection = document.createElement("div"); // Create a new div for each floorPlan
+    levelSection.classList.add("level-section");
+
+    let h1 = document.createElement("h1");
+    h1.innerText = floorPlan.name;
+    levelSection.append(h1);
+
+    let img = document.createElement("img");
+    img.src =
+      "https://portiaportia.github.io/json/images/house-plans/" +
+      floorPlan.image;
+    levelSection.append(img);
+    section2.append(levelSection);
+  });
+
+  superSection.append(section2);
+  console.log(superSection);
+
+  return superSection;
 };
 
 showHouses();
