05d6b49046a59d1bfa2b1d0c67f665564d9a8164
Gold3n-LCP <lukepalassis@gmail.com>
2024-03-16 21:11:15 -0400

    updated part5

--- projects/part5/info.js
+++ projects/part5/info.js
@@ -15,14 +15,13 @@ const showInfo = async () => {
   let index = infoSection.getAttribute("index-id");
   let album = info[index];
 
+  infoSection.append(getHead(album));
   infoSection.append(getInfo(album));
+  infoSection.append(getDescription(album));
 };
 
-const getInfo = (album) => {
-  let superSection = document.createElement("section");
-
+const getHead = (album) => {
   let head = document.createElement("section");
-
   let h1 = document.createElement("h1");
   h1.innerText = album.album_name;
   let h2 = document.createElement("h2");
@@ -30,57 +29,74 @@ const getInfo = (album) => {
 
   head.append(h1);
   head.append(h2);
-  superSection.append(head);
+  return head;
+};
 
-  let section = document.createElement("section");
-  section.setAttribute("id", "infoSection");
-  section.setAttribute("class", "columns");
+const getDescription = (album) => {
+  let description = document.createElement("section");
+
+  let h3 = document.createElement("h3");
+  h3.innerText = album.description;
+
+  description.append(h3);
+  return description;
+};
+
+const getInfo = (album) => {
+  let superSection = document.createElement("section");
+  superSection.setAttribute("class", "columns");
+
+  let section = document.createElement("div");
+  //section.setAttribute("class", "columns");
   section.classList.add("album");
+  section.classList.add("column");
 
   let img = document.createElement("img");
   img.src = "images/" + album.img_name;
   section.append(img);
 
-  const iframe = document.createElement(`iframe`);
+  let iframe = document.createElement("iframe");
   iframe.src = album.link;
-  iframe.width = "80%";
-  iframe.height = "80%";
+  console.log(iframe);
+  iframe.width = "60%";
+  iframe.height = "80px";
   iframe.title = "Dynamic Iframe";
   section.append(iframe);
+  section.append(document.createElement("br"));
+  const attribution = document.createElement("img");
+  attribution.setAttribute("id", "spotify");
+  attribution.src = "images/spotify.svg";
+  section.append(attribution);
+
+  superSection.append(section);
+  ////////////////////////////////
+  let section2 = document.createElement("div");
+  section2.setAttribute("class", "columns");
+  section2.classList.add("column");
 
   let h3 = document.createElement("h3");
-  h3.innerText =
-    "Album : " +
-    album.album_name +
-    "\n\nBand Name: " +
-    album.band_name +
-    "\n\nDescription: " +
-    album.description +
-    "\n\n";
+  h3.innerText = "Band Members:\n";
   for (let [role, name] of Object.entries(album.band_members)) {
     h3.append(`${role}:  ${name}`);
     h3.appendChild(document.createElement("br"));
   }
-  section.append(h3);
-
-  superSection.append(section);
-
-  let section2 = document.createElement("section");
-  section2.setAttribute("class", "columns");
+  section2.append(h3);
   let songsSection = document.createElement("section");
 
   let songs = Object.values(album.song_list);
+  let list = document.createElement("h3");
+  list.innerText = "Song List:\n";
   songs.forEach((song) => {
-    let songsSection = document.createElement("div"); // Create a new div for each floorPlan
+    // Create a new div for each floorPlan
     //songsSection.classList.add("level-section");
 
-    let h1 = document.createElement("h1");
-    h1.innerText = song;
-    songsSection.append(h1);
+    list.append(song);
+    list.appendChild(document.createElement("br"));
   });
+  songsSection.append(list);
+  section2.append(songsSection);
 
   superSection.append(section2);
-  console.log(superSection);
 
   return superSection;
 };
